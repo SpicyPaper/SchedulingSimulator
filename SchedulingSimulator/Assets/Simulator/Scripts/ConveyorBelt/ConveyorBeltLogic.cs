98e172ebf1a07b404ce1fe01c5e345275f92510f
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-02-25 09:47:40 +0100

    Continue conveyorbelt

--- SchedulingSimulator/Assets/Simulator/Scripts/ConveyorBelt/ConveyorBeltLogic.cs
+++ SchedulingSimulator/Assets/Simulator/Scripts/ConveyorBelt/ConveyorBeltLogic.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ConveyorBeltLogic : MonoBehaviour
 {
+    float scrollSpeed = 1.3f;
+    Renderer rend;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, -offset));
     }
 }
