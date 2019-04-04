6dc58ec3040c140e4eea10ee9e7dd206badad403
Kurokabe <farid.abdalla.13@gmail.com>
2019-04-04 16:59:03 +0200

    Add screens.

---
+++ SchedulingSimulator/Assets/Simulator/Scripts/UI/AddObjectToList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddObjectToList : MonoBehaviour
+{
+    public GameObject Content;
+    public GameObject ButtonPrefab;
+
+    public void AddItem()
+    {
+        GameObject button = Instantiate(ButtonPrefab);
+        button.transform.SetParent(Content.transform);
+        button.transform.localPosition = Vector3.zero;
+        button.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        
+
+        button.GetComponentInChildren<Text>().text = "test";
+    }
+}
