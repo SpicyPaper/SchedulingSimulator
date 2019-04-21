d351ac57cfbf216eddbd9d65dcdf42b59306dcab
Farid Abdalla <farid.abdalla.13@gmail.com>
2019-04-21 19:09:42 +0200

    Change pipe color when block is falling

---
+++ SchedulingSimulator/Assets/Simulator/Scripts/PipeColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeColor : MonoBehaviour
+{
+    public Color OnColor = Color.green;
+    public Color OffColor = Color.red;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", OnColor);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", OffColor);
+    }
+}
