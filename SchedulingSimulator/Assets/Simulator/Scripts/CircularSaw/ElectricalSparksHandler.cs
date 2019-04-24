de17cf285fdb147d018caea63b420540f220fabd
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-04-24 10:04:45 +0200

    Add particules effects

---
+++ SchedulingSimulator/Assets/Simulator/Scripts/CircularSaw/ElectricalSparksHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricalSparksHandler : MonoBehaviour
+{
+    [SerializeField]
+    private CircularSawHandler sawHandler;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sawHandler.IsSawTurning)
+        {
+            if (!GetComponent<ParticleSystem>().isPlaying)
+            {
+                GetComponent<ParticleSystem>().Play();
+            }
+        }
+        else
+        {
+            GetComponent<ParticleSystem>().Stop();
+        }
+    }
+}
